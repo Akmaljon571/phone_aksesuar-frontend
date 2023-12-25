47ce5ffe67b2d884f01a2241401648f60b9c6b6e
Akmaljon571 <akmaljon079@gmail.com>
2023-12-26 02:05:08 +0300

    finish: footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,53 @@
+import { Link } from 'react-router-dom'
+import logo from '../../img/Frame 9.svg'
+import vk from '../../img/VK.svg'
+import ins from '../../img/Instagram.svg'
+import tg from '../../img/Telegram.svg'
+import w from '../../img/Whatsapp.svg'
 import './footer.scss'
 
 function Footer() {
     return (
         <footer className="footer">
-            footer
+            <Link to={'/'}>
+                <img src={logo} alt="Company Logo" />
+            </Link>
+            <ul>
+                <li>
+                    Telefon
+                </li>
+                <li>
+                    Iphone
+                </li>
+                <li>
+                    Redmi
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    Oyna
+                </li>
+                <li>
+                    Ekran
+                </li>
+                <li>
+                    Maska
+                </li>
+            </ul>
+            <ul className='tarmoq'>
+                <li>
+                    <img src={vk} alt="Ichtimoiy Tarmoq" />
+                </li>
+                <li>
+                    <img src={ins} alt="Ichtimoiy Tarmoq" />
+                </li>
+                <li>
+                    <img src={tg} alt="Ichtimoiy Tarmoq" />
+                </li>
+                <li>
+                    <img src={w} alt="Ichtimoiy Tarmoq" />
+                </li>
+            </ul>
         </footer>
     )
 }
