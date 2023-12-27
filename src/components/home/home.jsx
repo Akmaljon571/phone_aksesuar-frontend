b4ea84346885928e0abb4827407bff9ff1855f2b
Akmaljon571 <akmaljon079@gmail.com>
2023-12-27 13:18:27 +0300

    finish: home

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,12 +1,11 @@
 import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Products from "../products/product";
 import { State, api } from "../../context";
-import { useNavigate } from "react-router-dom";
 
 function HomeProducts() {
     const [cat, setCat] = useState([]);
     const { user_id, count } = useContext(State)
-    const [arr, setArr] = useState([]);
     const [products, setProducts] = useState([]);
     const navigate = useNavigate()
 
@@ -22,11 +21,13 @@ function HomeProducts() {
 
     useEffect(() => {
         if (cat.length) {
+            let arr = []
             cat.map(e => (
                 fetch(api + `/product/${e.id}/user/${user_id}/`)
                     .then(re => re.json())
                     .then(data => {
-                        setProducts([data])
+                        setProducts([{ id: e.id, data: data.slice(0, 3) }, ...arr])
+                        arr.push({ id: e.id, data: data.slice(0, 3) })
                     })
             ))
         }
@@ -39,9 +40,9 @@ function HomeProducts() {
                 <div key={e.id}>
                     <h3 onClick={() => navigate('/category/' + e.id)} className="title">{e.title}</h3>
                     <ul className="products">
-                        {products[i]?.map((prod, index) => (
+                        {products.sort((a, b) => a.id - b.id)[i]?.id === e.id ? products.sort((a, b) => a.id - b.id)[i]?.data?.map((prod, index) => (
                             <Products key={prod.id} id={prod.id} image={prod.image} title={prod.title} price={prod.price} liked={prod.liked} ordered={prod.ordered} />
-                        ))}
+                        )) : null}
                     </ul>
                 </div>
             )) : null}
